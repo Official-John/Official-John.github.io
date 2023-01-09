d21023a44680d473249fd5148d04c3ed3ed64ba0
John Ayodele <96767428+Official-John@users.noreply.github.com>
2023-01-09 12:15:10 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function(){
 	"use strict";
 
 		// Preloader
-		$(window).on("load", function () {
+		$('header').on("load", function () {
 			$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
 			$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
 			$('body').delay(333);
